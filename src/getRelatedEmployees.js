a68a7199bdb7fc13bba4ad8486176ed14027efaf
Victor Mendes <victorutdsim7@gmail.com>
2022-07-28 18:21:47 -0300

    requisito 4: [refatoração] cria função para pegar os gerentes

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,10 +1,20 @@
 const data = require('../data/zoo_data');
 
-const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-const managers = [stephanieId, olaId, burlId];
 const { employees } = data;
+
+function getManagers() {
+  return employees.reduce((managers, currentPerson) => {
+    currentPerson.managers.forEach((element) => {
+      if (!managers.includes(element)) {
+        managers.push(element);
+      }
+    });
+    return managers;
+  }, []);
+}
+
+const managers = getManagers();
+console.log(managers);
 
 function isManager(id) {
   // seu código aqui
