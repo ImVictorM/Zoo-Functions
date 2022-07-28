540834e3e51b827be136b4da0685f2e6d22e315c
Victor Mendes <victorutdsim7@gmail.com>
2022-07-28 15:33:40 -0300

    requisito 3: completo

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui!
+  if (!employeeName) return {};
+  const { employees } = data;
+  return employees.find((employee) => {
+    const { firstName, lastName } = employee;
+    return firstName === employeeName || lastName === employeeName;
+  });
 }
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
