f64c3d574f74caf6890ba34214e933972dcaa301
Victor Mendes <victorutdsim7@gmail.com>
2022-07-28 15:14:23 -0300

    requisito 2: completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const chosenAnimal = data.species.find((animalObj) => animalObj.name === animal);
+  return chosenAnimal.residents.every((resident) => resident.age >= age);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
