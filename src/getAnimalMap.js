923d926a7cf42a1eb485320a63922298209112fc
Victor Mendes <victorutdsim7@gmail.com>
2022-07-29 19:26:27 -0300

    requisito 14: cria função que mapeia animais quando getAnimalsMap não recebe parâmetros

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+function mapAnimals(animals) {
+  return animals.reduce((map, currentAnimal) => {
+    const mapCopy = map;
+    const { location: currLocation, name: animalName } = currentAnimal;
+    if (!mapCopy[currLocation]) {
+      mapCopy[currLocation] = [];
+      mapCopy[currLocation].push(animalName);
+    } else {
+      mapCopy[currLocation].push(animalName);
+    }
+    return mapCopy;
+  }, {});
 }
+
+function getAnimalMap(options) {
+  if (!options) return mapAnimals(species);
+}
+
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
